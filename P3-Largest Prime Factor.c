4d3e02c0e2a15c2fe255355853ad60492f990c99
Rafael Melo <rsmelo_@hotmail.com>
2016-04-07 17:20:37 -0300

    Added files via upload

---
+++ P3-Largest Prime Factor.c
@@ -0,0 +1,30 @@
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+*/
+#include<stdio.h>
+
+void prime(unsigned long long pr){
+	unsigned long long i, maior=0,f,aux=1;
+	for(i=2;i<pr;i++){
+		f=pr%i;
+		if(f==0){
+			aux=i*aux;
+			if(maior<i){
+				maior=i;
+			}
+			if(aux>=pr){
+				break;
+			}
+		}
+	}
+	printf("Maior=%lu",maior);
+}
+
+int main(){
+	unsigned long long pr=600851475143;
+	prime(pr);
+	
+	return(0);
+}
